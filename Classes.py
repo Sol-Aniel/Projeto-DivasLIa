0b52b57dd40b02af84423cf1d0f565a4b55cf3c5
Sol Aniel <129297452+Sol-Aniel@users.noreply.github.com>
2024-03-15 11:39:02 -0300

    Classes.py

--- Classes.py
+++ Classes.py
@@ -1,38 +1,50 @@
-class Funcionários:
-    def __init__(self, Nome, CPF, Tel, Email, Endereço, Salário, CH, Código):
-        self.Nome = str(Nome)
-        self.CPF = str(CPF)
-        self.Tel = int(Tel)
-        self.Email = str(Email)
-        self.Endereço = str(Endereço)
-        self.Salário = float(Salário)
-        self.CH = float(CH)
-        self.Código = int(Código)
+lista_pessoa=[]
+lista_funcionario=[]
+lista_livros=[]
+lista_revistas=[]
+lista_jornais=[]
+lista_emprestimos=[]
 
-class Cliente:
-    def __init__(self, Nome, CPF, Tel, Email, Endereço):
+class Pessoa:
+    def __init__(self, Nome, CPF, Tel, Email, Endereco):
         self.Nome = str(Nome)
         self.CPF = str(CPF)
         self.Tel = int(Tel)
         self.Email = str(Email)
-        self.Endereço = str(Endereço)
+        self.Endereco = str(Endereco)
+
+class Funcionarios (Pessoa):
+    def __init__(self, Nome, CPF, Tel, Email, Endereco, Salario, CH, Codigo):
+        super().__init__(self, Nome, CPF, Tel, Email, Endereco)
+        self.Salario = float(Salario)
+        self.CH = float(CH)
+        self.Codigo = int(Codigo)
 
 class Documentos:
-    def __init__(self, Título, PubANO, Editora, Idioma, Disponibilidade):
-        self.Título = str(Título)
+    def __init__(self, Titulo, PubANO, Editora, Idioma, Disponibilidade):
+        self.Titulo = str(Titulo)
         self.PubANO = int(PubANO)
         self.Editora = str(Editora)
         self.Idioma = str(Idioma)
         self.Disponibilidade = bool(Disponibilidade)
 
 class Livros (Documentos):
-    def __init__(self, Título, Autor, PubANO, Editora, Edição, Disponibilidade):
-        super().__init__(Título, PubANO, Editora, Disponibilidade)
+    def __init__(self, Titulo, Autor, PubANO, Editora, Edição, Disponibilidade):
+        super().__init__(Titulo, PubANO, Editora, Disponibilidade)
         self.Autor = str(Autor)
         self.Edição = int(Edição)
 class Revistas (Documentos):
-    def __init__(self, Título, PubANO, Editora, Idioma, Disponibilidade):
-        super().__init__(Título, PubANO, Editora, Idioma, Disponibilidade)
+    def __init__(self, Titulo, PubANO, PubSEM, Editora, Idioma, Disponibilidade):
+        super().__init__(Titulo, PubANO, Editora, Idioma, Disponibilidade)
+        self.PubSEM = int(PubSEM)
 class Jornais (Documentos):
-    def __init__(self, Título, PubANO, Editora, Idioma, Disponibilidade):
-        super().__init__(Título, PubANO, Editora, Idioma, Disponibilidade)
+    def __init__(self, Titulo, PubANO, PubDIA, Editora, Idioma, Disponibilidade):
+        super().__init__(Titulo, PubANO, Editora, Idioma, Disponibilidade)
+        self.PubDIA = int(PubDIA)
+
+class Emprestimos:
+    def __init__(self, Cliente, Documento, DataRet, DataDev):
+        self.Cliente = str(Cliente)
+        self.Documento = str(Documento)
+        self.DataRet = str(DataRet)
+        self.DataDev = str(DataDev)
