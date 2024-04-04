dedc01888ab7702958cae1d4dc154102c32c8576
Any Gabriely <129224785+any-g@users.noreply.github.com>
2024-04-04 19:34:59 -0300

    Update Classes.py

--- Classes.py
+++ Classes.py
@@ -21,25 +21,26 @@ class Funcionarios (Pessoa):
         self.Codigo = int(Codigo)
 
 class Documentos:
-    def __init__(self, Titulo, PubANO, Editora, Idioma, Disponibilidade):
+    def __init__(self, Titulo, PubANO, Editora, Idioma, Disponibilidade, quantidade):
         self.Titulo = str(Titulo)
         self.PubANO = int(PubANO)
         self.Editora = str(Editora)
         self.Idioma = str(Idioma)
         self.Disponibilidade = bool(Disponibilidade)
+        self.quantidade=int(quantidade)
 
 class Livros (Documentos):
-    def __init__(self, Titulo, Autor, PubANO, Editora, Edição, Disponibilidade):
-        super().__init__(Titulo, PubANO, Editora, Disponibilidade)
+    def __init__(self, Titulo, PubANO, Editora, Idioma, Disponibilidade, quantidade):
+        super().__init__(self, Titulo, PubANO, Editora, Idioma, Disponibilidade, quantidade)
         self.Autor = str(Autor)
         self.Edição = int(Edição)
 class Revistas (Documentos):
-    def __init__(self, Titulo, PubANO, PubSEM, Editora, Idioma, Disponibilidade):
-        super().__init__(Titulo, PubANO, Editora, Idioma, Disponibilidade)
+    def __init__(self, Titulo, PubANO, Editora, Idioma, Disponibilidade, quantidade):
+        super().__init__(self, Titulo, PubANO, Editora, Idioma, Disponibilidade, quantidade)
         self.PubSEM = int(PubSEM)
 class Jornais (Documentos):
-    def __init__(self, Titulo, PubANO, PubDIA, Editora, Idioma, Disponibilidade):
-        super().__init__(Titulo, PubANO, Editora, Idioma, Disponibilidade)
+    (self, Titulo, PubANO, Editora, Idioma, Disponibilidade):
+        super().__init__(self, Titulo, PubANO, Editora, Idioma, Disponibilidade, quantidade):
         self.PubDIA = int(PubDIA)
 
 class Emprestimos:
@@ -48,3 +49,14 @@ class Emprestimos:
         self.Documento = str(Documento)
         self.DataRet = str(DataRet)
         self.DataDev = str(DataDev)
+
+livro1.livros=("Dom Quixote", 1605, "Editorial Clássicos","Miguel de Cervantes", " 1ª edição", 3)
+livro2.livros=("1984", 1949, "Penguin Books","George Orwell", " 1ª edição", 6)
+livro3.livros=("Orgulho e Preconceito", 1813," Penguin Classics","Jane Austen", "1ª edição", 2)
+livro4.livros=("Cem Anos de Solidão", 1967, "HarperCollins","Gabriel García Márquez", "1ª edição", 1)
+livro5.livros=(""Harry Potter e a Pedra Filosofal", 1997, "Bloomsbury Publishing","J.K Rolling", "1ª edição", 9)
+livro6.livros=("O Senhor dos Anéis: A Sociedade do Anel", 1954, "Houghton Mifflin","J.R.R. Tolkien", "1ª edição", 8)
+livro7.livros=("Crime e Castigo", 1866, "Editora 34","Fiódor Dostoiévski", "1ª edição", 5)
+livro8.livros=("A revolução dos Bixos", 1945, "Companhia das Letras","George Orwell", "1ª edição", 15)
+livro9.livros=("O Pequeno Príncipe", 1943, "Agir","Antoine de Saint-Exupéry", "1ª edição", 25)
+livro10.livros=("O Apanhador no Campo de Centeio", 1951, "Little, Brown and Company","J.D. Salinger", "1ª edição", 11)
