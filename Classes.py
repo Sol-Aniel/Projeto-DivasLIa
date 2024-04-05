5f50a77602bd9270bddfb879e6516a8ad6144a49
Any Gabriely <129224785+any-g@users.noreply.github.com>
2024-04-05 14:04:32 -0300

    Update Classes.py

--- Classes.py
+++ Classes.py
@@ -21,25 +21,26 @@ class Funcionarios (Pessoa):
         self.Codigo = int(Codigo)
 
 class Documentos:
-    def __init__(self, Titulo, PubANO, Editora, Disponibilidade, quantidade):
+    def __init__(self, Titulo, PubANO, Editora, Idioma, Disponibilidade, quantidade):
         self.Titulo = str(Titulo)
         self.PubANO = int(PubANO)
         self.Editora = str(Editora)
+        self.Idioma = str(Idioma)
         self.Disponibilidade = bool(Disponibilidade)
         self.quantidade=int(quantidade)
 
 class Livros (Documentos):
-    def __init__(self, Titulo, PubANO, Editora,  Disponibilidade, quantidade):
-        super().__init__(self, Titulo, PubANO, Editora,  Disponibilidade, quantidade)
+    def __init__(self, Titulo, PubANO, Editora, Idioma, Disponibilidade, quantidade):
+        super().__init__(self, Titulo, PubANO, Editora, Idioma, Disponibilidade, quantidade)
         self.Autor = str(Autor)
         self.Edição = int(Edição)
 class Revistas (Documentos):
-    def __init__(self, Titulo, PubANO, Editora, Disponibilidade, quantidade):
-        super().__init__(self, Titulo, PubANO, Editora, Disponibilidade, quantidade)
+    def __init__(self, Titulo, PubANO, Editora, Idioma, Disponibilidade, quantidade):
+        super().__init__(self, Titulo, PubANO, Editora, Idioma, Disponibilidade, quantidade)
         self.PubSEM = int(PubSEM)
 class Jornais (Documentos):
     (self, Titulo, PubANO, Editora, Idioma, Disponibilidade):
-        super().__init__(self, Titulo, PubANO, Editora, Disponibilidade, quantidade):
+        super().__init__(self, Titulo, PubANO, Editora, Idioma, Disponibilidade, quantidade):
         self.PubDIA = int(PubDIA)
 
 class Emprestimos:
@@ -61,18 +62,18 @@ livro9=Livro("O Pequeno Príncipe", 1943, "Agir","Antoine de Saint-Exupéry", "1
 livro10=Livro("O Apanhador no Campo de Centeio", 1951, "Little, Brown and Company","J.D. Salinger", "1ª edição", 11)
 
 revista1=Revistas("Nactional Geographic",'National Geographic Society' 2022, 1)
-revista2=Revistas()
-revista3=Revistas()
-revista4=Revistas()
-revista5=Revistas()
+revista2=Revistas("Time", "Time USA LLC", 2024, 9)
+revista3=Revistas("Scientific American" ,"Springer Nature", 2024, 16)
+revista4=Revistas("The Economist" ,"The Economist Group" ,2023, 7)
+revista5=Revistas("Vogue","Condé Nast", 2022, 13)
 
-jornal1=Jornais()
-jornal1=Jornais()
-jornal1=Jornais()
-jornal1=Jornais()
-jornal1=Jornais()
-jornal1=Jornais()
-jornal1=Jornais()
+jornal1=Jornais("The New York Times", "The New York Times Company", 2024, 25)
+jornal2=Jornais("The Guardian", "Guardian Media Group", 2024, 37)
+jornal3=Jornais("Le Monde", "Groupe Le Monde", 2024, 28)
+jornal4=Jornais("The Times", "News UK", 2024, 53)
+jornal5=Jornais("El País", "PRISA", 2024, 13)
+jornal6=Jornais("Der Spiegel", "Spiegel-Verlag", 2024, 17)
+jornal7=Jornais("Yomiuri Shimbun", "The Yomiuri Shimbun Holdings", 2024, 43)
 
 lista_livros.append('livro1','livro2', 'livro3', 'livro4', 'livro5', 'livro6','livro7','livro8', 'livro9','livro10')
 lista_revistas.append('revista1','revista2','revista3','revista4','revista5')
